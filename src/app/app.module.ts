805bc01a0caa74f8282500b43122eba18d6edf25
rxmxdd00 <albania.reiner20@gmail.com>
2024-03-03 13:38:41 +0800

    updating assets and configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { CourseComponent } from './pages/course/course.component';
+import { DepartmentComponent } from './pages/department/department.component';
+import { EnrollmentComponent } from './pages/enrollment/enrollment.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { StudentComponent } from './pages/student/student.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccordionComponent
+    AccordionComponent,
+    DepartmentComponent,
+    StudentComponent,
+    CourseComponent,
+    TeacherComponent,
+    EnrollmentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CdkAccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]

23ebea744bc6ab4335a48151741eaaef437d9365
rxmxdd00 <albania.reiner20@gmail.com>
2024-03-06 18:14:07 +0800

    update interface for application of relational

--- src/app/interface/course.ts
+++ src/app/interface/course.ts
@@ -2,6 +2,7 @@ export interface Course {
     id : Number,
     courseName : string,
     departmentId:string,
+    departmentName: string,
     created_at : string,
     updated_at:string
 }
--- src/app/interface/enrollment.ts
+++ src/app/interface/enrollment.ts
@@ -2,7 +2,13 @@ export interface Enrollment {
     id : string,
     studentId : string,
     courseId : string,
+    departmentId: string,
     enrollment_date : string,
+    DOB : string,
+    address : string,
+    firstName : string,
+    lastName : string,
+    courseName : string,
     created_at : string,
-    updated_at:string
+    updated_at:string,
 }
